babe9bc94a4cbef669308db14fb6c14accd8537b
cedoor <me@cedoor.dev>
2021-10-19 12:21:07 +0200

    refactor: set up @libsem/types dep
    Former-commit-id: afffa7e5a928db57bf3d87c8afd6a8f8b70d5dfd [formerly 5ea76cb8f1cca1c11044c8525dddc4b1d443c77f]
    Former-commit-id: fedd6b143c2883ea0c121f53418e2d9814811b2f

--- packages/protocols/src/semaphore.ts
+++ packages/protocols/src/semaphore.ts
@@ -1,23 +1,29 @@
 import { ZkProtocol } from "./zk-protocol";
 import { genSignalHash, poseidonHash } from "./utils";
-import { Identity, MerkleProof } from '../../types';
+import { Identity, MerkleProof } from "@libsem/types";
 
 class Semaphore extends ZkProtocol {
-    genWitness(identity: Identity, merkleProof: MerkleProof, externalNullifier: string | bigint, signal: string, shouldHash = true): any {
-        return {
-            identity_nullifier: identity.identityNullifier,
-            identity_trapdoor: identity.identityTrapdoor,
-            identity_path_index: merkleProof.indices,
-            path_elements: merkleProof.pathElements,
-            external_nullifier: externalNullifier,
-            signal_hash: shouldHash ? genSignalHash(signal): signal,
-        }
-    }
+  genWitness(
+    identity: Identity,
+    merkleProof: MerkleProof,
+    externalNullifier: string | bigint,
+    signal: string,
+    shouldHash = true
+  ): any {
+    return {
+      identity_nullifier: identity.identityNullifier,
+      identity_trapdoor: identity.identityTrapdoor,
+      identity_path_index: merkleProof.indices,
+      path_elements: merkleProof.pathElements,
+      external_nullifier: externalNullifier,
+      signal_hash: shouldHash ? genSignalHash(signal) : signal,
+    };
+  }
 
-    genNullifierHash(externalNullifier: string | bigint, identityNullifier: string | bigint, nLevels: number): bigint {
-        return poseidonHash([BigInt(externalNullifier), BigInt(identityNullifier), BigInt(nLevels)]);
-    }
-
+  genNullifierHash(externalNullifier: string | bigint, identityNullifier: string | bigint, nLevels: number): bigint {
+    return poseidonHash([BigInt(externalNullifier), BigInt(identityNullifier), BigInt(nLevels)]);
+  }
 }
 
 export default new Semaphore();
+
