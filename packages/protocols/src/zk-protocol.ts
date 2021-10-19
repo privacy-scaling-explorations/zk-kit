2ffe9c73caecfd2444a1123d41749bd382002278
Andrija Novakovic <andrijanovakovic@Andrijas-MacBook-Pro.local>
2021-10-19 17:10:13 +0200

    functions comments

--- packages/protocols/src/zk-protocol.ts
+++ packages/protocols/src/zk-protocol.ts
@@ -4,15 +4,33 @@ import { SNARK_FIELD_SIZE } from "./utils";
 import { IProof } from "@libsem/types";
 
 export class ZkProtocol {
+  /**
+   * Generates full proof
+   * @param grothInput witness
+   * @param wasmFilePath path to wasm file
+   * @param finalZkeyPath path to final zkey file
+   * @returns zero knowledge proof
+   */
   genProof(grothInput: any, wasmFilePath: string, finalZkeyPath: string): Promise<IProof> {
     return groth16.fullProve(grothInput, wasmFilePath, finalZkeyPath);
   }
 
+  /**
+   * Verify ZK proof
+   * @param vKey verifikation key
+   * @param fullProof proof
+   * @returns Is provided proof valid
+   */
   verifyProof(vKey: string, fullProof: IProof): Promise<boolean> {
     const { proof, publicSignals } = fullProof;
     return groth16.verify(vKey, publicSignals, proof);
   }
 
+  /**
+   * Transforms proof that can be compatible with solidity input
+   * @param fullProof
+   * @returns Proof
+   */
   packToSolidityProof(fullProof: IProof) {
     const { proof, publicSignals } = fullProof;
 
