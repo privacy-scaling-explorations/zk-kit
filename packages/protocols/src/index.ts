f9556bdf17d507350ba516b2b43349f0b93b2057
Andrija Novakovic <andrijanovakovic@Andrijas-MacBook-Pro.local>
2021-10-19 15:49:14 +0200

    export types
    Former-commit-id: 796567063273bcaa77a579409d445020d328bac1 [formerly f0ebe86a71e9771ebc76e274536e76ca4fe02d0e]
    Former-commit-id: 21bf59b5542ad7691bf7e19604d03585afb74871

--- packages/protocols/src/index.ts
+++ packages/protocols/src/index.ts
@@ -2,10 +2,15 @@ import Semaphore from './semaphore';
 import Rln from './rln';
 import NRln from './nRln';
 import { generateMerkleProof } from './utils';
+import { Identity, MerkleProof, IProof } from "@libsem/types";
 
-export { 
+
+export {
     Semaphore,
-    Rln, 
+    Rln,
     NRln,
-    generateMerkleProof
+    generateMerkleProof,
+    Identity,
+    MerkleProof,
+    IProof
 }
