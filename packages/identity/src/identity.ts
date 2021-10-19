d362e471da110db574761ebfd70afc4190953fde
Andrija Novakovic <andrijanovakovic@Andrijas-MacBook-Pro.local>
2021-10-19 16:19:41 +0200

    comments in identity lib

--- packages/identity/src/identity.ts
+++ packages/identity/src/identity.ts
@@ -8,6 +8,12 @@ const poseidonHash = (data: Array<bigint>): bigint => {
 };
 
 class ZkIdentity {
+  /**
+   * Generates new ZkIdentity
+   * @param strategy strategy for identity generation
+   * @param metadata additional data needed to create identity for given strategy
+   * @returns Identity
+   */
   genIdentity(strategy = "random", metadata: any = {}): Identity {
     if (strategy === "random") return genRandomIdentity();
     else if (strategy === "signedMessage") return genIdentityFromSignedMessage(metadata);
@@ -15,10 +21,20 @@ class ZkIdentity {
     throw new Error("provided strategy is not supported");
   }
 
+  /**
+   * Creates secret from ZkIdentity
+   * @param identity identity to generate secret for
+   * @returns secret
+   */
   genSecretFromIdentity(identity: Identity): bigint[] {
     return [identity.identityNullifier, identity.identityTrapdoor];
   }
 
+  /**
+   * Creates random secret
+   * @param parts number of parts in secret
+   * @returns secret
+   */
   genRandomSecret(parts = 2): bigint[] {
     const secret: bigint[] = [];
     for (let i = 0; i < parts; i++) {
@@ -27,16 +43,31 @@ class ZkIdentity {
     return secret;
   }
 
+  /**
+   * Generate commitment from identity secret
+   * @param secret identity secret
+   * @returns identity commitment
+   */
   genIdentityCommitment(secret: bigint[]): bigint {
     const secretHash = poseidonHash(secret);
     return poseidonHash([secretHash]);
   }
 
+  /**
+   * Serializes identity
+   * @param identity to serialize
+   * @returns serialized identity
+   */
   serializeIdentity(identity: Identity): string {
     const data = [identity.identityNullifier.toString(16), identity.identityTrapdoor.toString(16)];
     return JSON.stringify(data);
   }
 
+  /**
+   * Unserializes identity
+   * @param serialisedIdentity
+   * @returns ZkIdentity
+   */
   unSerializeIdentity(serialisedIdentity: string): Identity {
     const data = JSON.parse(serialisedIdentity);
     return {
