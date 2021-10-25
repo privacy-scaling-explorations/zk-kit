478bb835df06d25386c9be3b25b1702a9a42d246
Andrija Novakovic <andrijanovakovic@Andrijas-MacBook-Pro.local>
2021-10-25 11:20:36 +0200

    serialized identity type
    Former-commit-id: fc442727c89fe8fff11dffc27e9f84b3cbecb133 [formerly 49df65929d411b0c72063c244f195ba66c81742a]
    Former-commit-id: 7a6cb3b79a4b33a0e61b6027bbbb2468843e5103

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -14,5 +14,4 @@ export interface MerkleProof {
   pathElements: Array<any>
 }
 
-export type IdentityFactoryMetadata = {} | { signedMessage: string } |
-{ identityNullifier: bigint, identityTrapdoor: bigint }
+export type SerializedIdentity = { identityNullifier: string, identityTrapdoor: string };
