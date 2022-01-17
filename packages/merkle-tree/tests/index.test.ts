e41a41188be54a479334ccb3b78418506f735b23
Andrija <akinovak@gmail.com>
2022-01-17 21:31:34 +0100

    feat: nary wrapper around binary tree
    Former-commit-id: fccf552dc73a2292b2988b74b8ddca4f41cb0f95 [formerly e325a6981e87ab19f71d0a0e00c22e681fb3e709] [formerly c6bef0eddfc3d1a8c7f1bf238ef15b11c14e1388 [formerly 88d01b8c38bd45cd14e9f8616d4e44e65b85f001]]
    Former-commit-id: 4d7ea3377f06cbba4bf2e79b360ec30642888ea9 [formerly f3991f128196a6f31c19cd054dfa28a03406693c]
    Former-commit-id: ae385070fc52107cdc84e29d550b61219d1b7178

--- packages/merkle-tree/tests/index.test.ts
+++ packages/merkle-tree/tests/index.test.ts
@@ -1,29 +1,31 @@
 import { poseidon } from "circomlibjs"
-import { NAryIncrementalTree } from "../src"
+import { IncrementalMerkleTree } from "../src"
 
 describe("Nary Merkle Tree", () => {
-  const depth = 20;
-  const arity = 5;
-  let tree: NAryIncrementalTree;
+  const depth = 20
+  const arity = 5
+  let tree: IncrementalMerkleTree
 
   describe("Merkle Tree class", () => {
     beforeEach(() => {
-      tree = new NAryIncrementalTree(poseidon, depth, BigInt(0), arity)
+      tree = new IncrementalMerkleTree(poseidon, depth, BigInt(0), arity)
     })
 
     it("Should not initialize a Merkle tree with wrong parameters", () => {
-      expect(() => new NAryIncrementalTree(undefined as any, 33, 0, arity)).toThrow("Parameter 'hash' is not defined")
-      expect(() => new NAryIncrementalTree(1 as any, 33, 0, arity)).toThrow("Parameter 'hash' is none of these types: function")
+      expect(() => new IncrementalMerkleTree(undefined as any, 33, 0, arity)).toThrow("Parameter 'hash' is not defined")
+      expect(() => new IncrementalMerkleTree(1 as any, 33, 0, arity)).toThrow(
+        "Parameter 'hash' is none of these types: function"
+      )
     })
 
     it("Should not initialize a Merkle tree with depth > 32", () => {
-      expect(() => new NAryIncrementalTree(poseidon, 33, BigInt(0), arity)).toThrow("The tree depth must be between 1 and 32")
+      expect(() => new IncrementalMerkleTree(poseidon, 33, BigInt(0), arity)).toThrow(
+        "The tree depth must be between 1 and 32"
+      )
     })
 
     it("Should initialize a Merkle tree", () => {
-      // console.log(tree.root)
       expect(tree.depth).toEqual(depth)
-      // expect(tree.root).toEqual(BigInt("3315762791558236426429898223445373782079540514426385620818139644150484427120n"))
       expect(tree.leaves).toHaveLength(0)
       expect(tree.zeroes).toHaveLength(depth)
       expect(tree.arity).toEqual(arity)
@@ -34,7 +36,7 @@ describe("Nary Merkle Tree", () => {
     })
 
     it("Should not insert a leaf in a full tree", () => {
-      const fullTree = new NAryIncrementalTree(poseidon, 1, BigInt(0), 3)
+      const fullTree = new IncrementalMerkleTree(poseidon, 1, BigInt(0), 3)
 
       fullTree.insert(BigInt(1))
       fullTree.insert(BigInt(2))
@@ -45,9 +47,9 @@ describe("Nary Merkle Tree", () => {
   })
 
   it("Should create a valid proof", () => {
-    const numberOfLeaves = 50;
+    const numberOfLeaves = 50
 
-    for (let i = 0; i < numberOfLeaves; i+=1) {
+    for (let i = 0; i < numberOfLeaves; i += 1) {
       tree.insert(BigInt(i + 1))
     }
 
