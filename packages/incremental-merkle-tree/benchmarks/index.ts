67d40fff3cb757203016e2d699c4d5b1710bf5ce
cedoor <me@cedoor.dev>
2022-01-17 21:58:15 +0100

    refactor: update pkg name

---
+++ packages/incremental-merkle-tree/benchmarks/index.ts
@@ -0,0 +1,39 @@
+import b from "benny"
+import { poseidon } from "circomlibjs"
+import { IncrementalMerkleTree } from "../src"
+import { IncrementalQuinTree } from "incrementalquintree"
+
+const newTree = new IncrementalMerkleTree(poseidon, 20, BigInt(0), 2)
+const oldTree = new IncrementalQuinTree(20, BigInt(0), 2, poseidon)
+
+const numberOfLeaves = 2 ** 4
+
+b.suite(
+  "Incremental Merkle tree benchmarks",
+
+  b.add("Generate proof for new tree", () => {
+    for (let i = 0; i < numberOfLeaves; i += 1) {
+      newTree.insert(BigInt(i + 1))
+    }
+
+    for (let i = 0; i < numberOfLeaves; i += 1) {
+      newTree.createProof(i)
+    }
+  }),
+
+  b.add("Generate proof for old tree", () => {
+    for (let i = 0; i < numberOfLeaves; i += 1) {
+      oldTree.insert(BigInt(i + 1))
+    }
+
+    for (let i = 0; i < numberOfLeaves; i += 1) {
+      oldTree.genMerklePath(i)
+    }
+  }),
+
+  b.cycle(),
+  b.complete(),
+
+  b.save({ file: "reduce", version: "1.0.0", details: true }),
+  b.save({ file: "reduce", format: "chart.html", details: true })
+)
