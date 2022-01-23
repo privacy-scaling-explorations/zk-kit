aded04b9199bbdfcc2d1b1f2f43e803f5429d646
cedoor <me@cedoor.dev>
2022-01-23 14:10:32 +0100

    chore: move benchmarks in the root folder

---
+++ benchmarks/index.ts
@@ -0,0 +1,13 @@
+import runIncrementalMerkleTree from "./incremental-merkle-tree"
+
+const [benchmark] = process.argv.slice(2)
+
+// If there is an argument with a specific benchmark to run, it will run only that
+// benchmark, otherwise it will run all the benchmarks.
+switch (benchmark) {
+  case "incremental-merkle-tree":
+    runIncrementalMerkleTree()
+    break
+  default:
+    runIncrementalMerkleTree()
+}
