65bc6defb9a2cf825a0e32bbb137d66025395020
cedoor <me@cedoor.dev>
2022-02-02 17:40:13 +0100

    feat: add incremental quin tree lib
    Former-commit-id: 71a0ceab075b7f94ae206377454a5bbdf2fa87c1 [formerly e797e6a18474dd25db61ca5d0ca227eab7f0c3fd] [formerly 08b1dcfa57e4c17b7f0bd115e40f41bee760fc09 [formerly 07c82a2805e3fcf3ec92370d92180c7bf36dc354]] [formerly 622525c151a38fc968ac60284cd485b620a3bb37 [formerly ccb7575e7ae50eb8f35375c390cc0d7514357bab] [formerly e72fe9cdedfc975f0497b7dd9af62c64a99fac55 [formerly 9ca54897ec99c82c65a0db9163f4267f329693b7]]]
    Former-commit-id: 71a9a6b2bccdf06c5190d76783dd6bab2e26eb7e [formerly 3889e12e11548c63d2d5a4c6fb8efe77aa0c6569] [formerly fe740d5489e115b4de255168a644455de4e3173a [formerly 8e173aa030a1b7d22a3cd3cae038e5667de07b2a]]
    Former-commit-id: 33348437ab6505fc3ef484fca392682411e56da4 [formerly 14f81bea6d7a88504f3fdc7abd2d9a0a72bfbb69]
    Former-commit-id: 9411242be84c82616fe0a588dcb96da59591707b

---
+++ packages/incremental-merkle-tree.sol/contracts/QuinTreeTest.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+import "./IncrementalQuinTree.sol";
+
+contract QuinTreeTest {
+  using IncrementalQuinTree for IncrementalTreeData;
+
+  event TreeCreated(bytes32 id, uint8 depth);
+  event LeafInserted(bytes32 indexed treeId, uint256 leaf, uint256 root);
+  event LeafRemoved(bytes32 indexed treeId, uint256 leaf, uint256 root);
+
+  mapping(bytes32 => IncrementalTreeData) public trees;
+
+  function createTree(bytes32 _id, uint8 _depth) external {
+    require(trees[_id].depth == 0, "QuinTreeTest: tree already exists");
+
+    trees[_id].init(_depth, 0);
+
+    emit TreeCreated(_id, _depth);
+  }
+
+  function insertLeaf(bytes32 _treeId, uint256 _leaf) external {
+    require(trees[_treeId].depth != 0, "QuinTreeTest: tree does not exist");
+
+    trees[_treeId].insert(_leaf);
+
+    emit LeafInserted(_treeId, _leaf, trees[_treeId].root);
+  }
+
+  function removeLeaf(
+    bytes32 _treeId,
+    uint256 _leaf,
+    uint256[4][] memory _proofSiblings,
+    uint8[] memory _proofPathIndices
+  ) external {
+    require(trees[_treeId].depth != 0, "QuinTreeTest: tree does not exist");
+
+    trees[_treeId].remove(_leaf, _proofSiblings, _proofPathIndices);
+
+    emit LeafRemoved(_treeId, _leaf, trees[_treeId].root);
+  }
+}
