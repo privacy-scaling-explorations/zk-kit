768d1800966c7a2bd083952ff836772c9e3d502d
cedoor <me@cedoor.dev>
2022-02-02 17:45:25 +0100

    fix: copy array before update object

--- packages/incremental-merkle-tree/src/verifyProof.ts
+++ packages/incremental-merkle-tree/src/verifyProof.ts
@@ -12,10 +12,11 @@ export default function verifyProof(proof: MerkleProof, hash: HashFunction): boo
   let node = proof.leaf
 
   for (let i = 0; i < proof.siblings.length; i += 1) {
-    // TODO: bug splice update the structure of the object
-    proof.siblings[i].splice(proof.pathIndices[i], 0, node)
+    const children = proof.siblings[i].slice()
 
-    node = hash(proof.siblings[i])
+    children.splice(proof.pathIndices[i], 0, node)
+
+    node = hash(children)
   }
 
   return proof.root === node
