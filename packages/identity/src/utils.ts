69cc68f225a363a3d8fdf4ac3e7108ca4ff1e8ce
cedoor <me@cedoor.dev>
2022-02-10 19:46:24 +0100

    refactor: remove unused ffjavascript dep
    Former-commit-id: e7553cbb202edc27251fb6455cd0e0ce12b08c02 [formerly 9c5f6c35604aa86aa5eb9ace8a241e3b10c39862] [formerly f7d59dbdac98f2d4c8d9e2e992ed8abb248bb7a7 [formerly 33c970727a7135ba1384eb2b457b933be8d329c5]] [formerly 2dfb907cc2163d4e6862d66eb4aa3867db1672b2 [formerly ec316c166f97b1983bf6d2e61443f314a8550229] [formerly 884b9cfc2dec0999e12122fea78aa44f392d7c41 [formerly 1e80cce7b6a421df6d1a0dc136688a0949ddf839]]]
    Former-commit-id: a54f2bd6ae9aa8435625568b3fa2311f774f31a9 [formerly c625b9225b0866482af41d34c2f06314ea0bf521] [formerly 8e26ceb9bf07ac4b3d1dc0b63b1565dd33ce6b22 [formerly cb1acbd7480ba084a4b68753ffbba3b03b9d6385]]
    Former-commit-id: 1af7626b2928668d45ca39304bffadfd5aeb4dd9 [formerly b2187677461985f67662bfc4c116a5477e6d5978]
    Former-commit-id: 7de276edc1e3419056252c212a18425effed90eb

--- packages/identity/src/utils.ts
+++ packages/identity/src/utils.ts
@@ -1,11 +1,6 @@
 import { BigNumber } from "@ethersproject/bignumber"
 import { randomBytes } from "@ethersproject/random"
-import { ZqField } from "ffjavascript"
 import { sha256 as _sha256 } from "js-sha256"
-
-const SNARK_FIELD_SIZE = BigInt("21888242871839275222246405745257275088548364400416034343698204186575808495617")
-
-export const Fq = new ZqField(SNARK_FIELD_SIZE)
 
 /**
  * Returns an hexadecimal sha256 hash of the message passed as parameter.
