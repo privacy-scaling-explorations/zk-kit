5a83b1501b1bf92b219a6f32ec49fb0a28fcdd10
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-04-18 11:05:48 +0800

    refactor(hashtower): rename _events[] to _fullLevels[]

--- packages/hashtower/src/proof-builder.ts
+++ packages/hashtower/src/proof-builder.ts
@@ -12,7 +12,7 @@ export default class HashTowerHashChainProofBuilder {
     private readonly _bitsPerLevel: number
     private readonly _digestFunc: (vs: Value[]) => Value
     private readonly _levels: Value[][]
-    private readonly _events: Value[][]
+    private readonly _fullLevels: Value[][]
 
     // TODO: doc
     // H does not have to be the same with the H of the contract
@@ -26,7 +26,7 @@ export default class HashTowerHashChainProofBuilder {
         this._bitsPerLevel = bitsPerLevel
         this._digestFunc = (vs: Value[]) => vs.reduce((acc, v) => hash([acc, v]))
         this._levels = []
-        this._events = []
+        this._fullLevels = []
     }
 
     public add(item: Value) {
@@ -39,10 +39,10 @@ export default class HashTowerHashChainProofBuilder {
         }
         if (lv === this._levels.length) {
             this._levels.push([])
-            this._events.push([])
+            this._fullLevels.push([])
         }
 
-        this._events[lv].push(toAdd)
+        this._fullLevels[lv].push(toAdd)
 
         const level = this._levels[lv]
         if (level.length < this._W) {
@@ -55,7 +55,7 @@ export default class HashTowerHashChainProofBuilder {
 
     public indexOf(item: Value) {
         checkParameter(item, "item", "bigint")
-        return this._events[0].indexOf(item)
+        return this._fullLevels[0].indexOf(item)
     }
 
     public build(idx: number): HashTowerHashChainProof {
@@ -63,10 +63,10 @@ export default class HashTowerHashChainProofBuilder {
         if (this._levels.length === 0) {
             throw new Error("The tower is empty.")
         }
-        if (idx < 0 || idx >= this._events[0].length) {
+        if (idx < 0 || idx >= this._fullLevels[0].length) {
             throw new Error(`Index out of range: ${idx}`)
         }
-        const item = this._events[0][idx]
+        const item = this._fullLevels[0][idx]
         let digests = this._levels.map(this._digestFunc)
         const digestOfDigests = this._digestFunc(digests.reverse())
         const levelLengths = this._levels.map((level, lv) => level.length << (this._bitsPerLevel * lv)).reduce(sum)
@@ -74,17 +74,17 @@ export default class HashTowerHashChainProofBuilder {
         const W = this._W
         let childrens = []
         for (let lv = 0; ; lv += 1) {
-            const levelStart = this._events[lv].length - this._levels[lv].length
+            const levelStart = this._fullLevels[lv].length - this._levels[lv].length
             const start = idx - (idx % W)
             if (start === levelStart) {
                 // we are in the tower now
                 digests = pad0(digests, H)
                 const rootLv = lv
-                const rootLevel = pad0(this._events[lv].slice(start, start + this._levels[lv].length), W)
+                const rootLevel = pad0(this._fullLevels[lv].slice(start, start + this._levels[lv].length), W)
                 childrens = pad00(childrens, H, W)
                 return { levelLengths, digestOfDigests, digests, rootLv, rootLevel, childrens, item }
             }
-            childrens.push(this._events[lv].slice(start, start + W))
+            childrens.push(this._fullLevels[lv].slice(start, start + W))
             idx = Math.floor(idx / W)
         }
     }
