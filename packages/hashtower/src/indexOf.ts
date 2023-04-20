b1cc6d7fc0b91f0f661c31e84d43de7a3e0d6986
Luoh Ren-Shan (LCamel) <lcamel@gmail.com>
2023-04-20 17:59:00 +0800

    revert(hashtower): go back to the function style for merging multiple files
    This reverts commit 1d75326eafda75bd7df31dd5773aea9622bd4470.

--- packages/hashtower/src/indexOf.ts
+++
@@ -1,6 +0,0 @@
-import checkParameter from "./checkParameter"
-
-export default function _indexOf(item: BigInt, fullLevels: BigInt[][]) {
-    checkParameter(item, "item", "bigint")
-    return fullLevels[0].indexOf(item)
-}
