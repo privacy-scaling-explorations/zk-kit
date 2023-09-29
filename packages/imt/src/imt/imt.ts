8c37006c5d52a93bd44d06701bf9bd9c658d4fea
cedoor <me@cedoor.dev>
2023-09-29 13:05:03 +0100

    docs(imt): add comments to imt implementation

--- packages/imt/src/imt/imt.ts
+++ packages/imt/src/imt/imt.ts
@@ -5,26 +5,50 @@ import { IMTHashFunction, IMTMerkleProof, IMTNode } from "./types"
  * An {@link IMT} (aka Incremental Merkle Tree) is a type of data structure used in cryptography and
  * computer science for efficiently verifying the integrity of a large set of data,
  * especially in situations where new data is added over time. It is based on the concept
- * of a Merkle tree, bit its key feature is its ability to efficiently update the tree
+ * of a Merkle tree, and its key feature is its ability to efficiently update the tree
  * when new data is added or existing data is modified.
- * In this implementation, the tree is constructed using a fixed maximum {@link IMT#depth}
+ * In this implementation, the tree is constructed using a fixed {@link IMT#depth}
  * value, and a list of {@link IMT#zeroes} (one for each level) is used to compute the
  * hash of a node when not all of its children are defined. The number of children for each
- * node can also be specified with {@link IMT#arity}.
+ * node can also be specified with the {@link IMT#arity} parameter.
  */
 export default class IMT {
+    /**
+     * The matrix where all the tree nodes are stored. The first index indicates
+     * the level of the tree, while the second index represents the node's
+     * position within that specific level.
+     */
     private readonly _nodes: IMTNode[][]
+    /**
+     * A list of zero values calculated during the initialization of the tree.
+     * The list contains one value for each level of the tree, and the value for
+     * a given level is equal to the hash of the previous level's value.
+     * The first value is the zero hash provided by the user.
+     * These values are used to calculate the hash of a node in case some of its
+     * children are missing.
+     */
     private readonly _zeroes: IMTNode[]
+    /**
+     * The hash function used to compute the tree nodes.
+     */
     private readonly _hash: IMTHashFunction
+    /**
+     * The depth of the tree, which is the number of edges from the node to the
+     * tree's root node.
+     */
     private readonly _depth: number
+    /**
+     * The number of children per node.
+     */
     private readonly _arity: number
 
     /**
-     * Initializes the tree with an hash function, the depth, the zero value to use for zeroes
-     * and the arity (i.e. the number of children for each node).
-     * @param hash Hash function.
-     * @param depth Tree depth.
-     * @param zeroValue Zero values for zeroes.
+     * It initializes the tree with an hash function, the depth, the zero value to use for zeroes
+     * and the arity (i.e. the number of children for each node). It also takes an optional parameter
+     * to initialize the tree with a list of leaves.
+     * @param hash The hash function used to create nodes.
+     * @param depth The tree depth.
+     * @param zeroValue The zero value used to create zeroes.
      * @param arity The number of children for each node.
      * @param leaves The list of initial leaves.
      */
@@ -82,49 +106,53 @@ export default class IMT {
     }
 
     /**
-     * Returns the root hash of the tree.
-     * @returns Root hash.
+     * The root of the tree. This value doesn't need to be stored as
+     * it is always the first and unique element of the last level of the tree.
+     * Its value can be retrieved in {@link IMT#_nodes}.
+     * @returns The root hash of the tree.
      */
     public get root(): IMTNode {
         return this._nodes[this.depth][0]
     }
 
     /**
-     * Returns the depth of the tree.
-     * @returns Tree depth.
+     * The depth of the tree, which equals the number of levels - 1.
+     * @returns The depth of the tree.
      */
     public get depth(): number {
         return this._depth
     }
 
     /**
-     * Returns the leaves of the tree.
-     * @returns List of leaves.
+     * The leaves of the tree. They can be retrieved from the first
+     * level of the tree using {@link IMT#_nodes}. The returned
+     * value is a copy of the array and not the original object.
+     * @returns The list of tree leaves.
      */
     public get leaves(): IMTNode[] {
         return this._nodes[0].slice()
     }
 
     /**
-     * Returns the zeroes nodes of the tree.
-     * @returns List of zeroes.
+     * The list of zero values calculated during the initialization of the tree.
+     * @returns The list of pre-computed zeroes.
      */
     public get zeroes(): IMTNode[] {
         return this._zeroes
     }
 
     /**
-     * Returns the number of children for each node.
-     * @returns Number of children per node.
+     * The number of children per node.
+     * @returns The number of children per node.
      */
     public get arity(): number {
         return this._arity
     }
 
     /**
-     * Returns the index of a leaf. If the leaf does not exist it returns -1.
-     * @param leaf Tree leaf.
-     * @returns Index of the leaf.
+     * It returns the index of a leaf. If the leaf does not exist it returns -1.
+     * @param leaf A leaf of the tree.
+     * @returns The index of the leaf.
      */
     public indexOf(leaf: IMTNode): number {
         checkParameter(leaf, "leaf", "number", "string", "bigint")
@@ -133,8 +161,14 @@ export default class IMT {
     }
 
     /**
-     * Inserts a new leaf in the tree.
-     * @param leaf New leaf.
+     * The leaves are inserted incrementally. If 'i' is the index of the last
+     * leaf, the new one will be inserted at position 'i + 1'. Every time a
+     * new leaf is inserted, the nodes that separate the new leaf from the root
+     * of the tree are created or updated if they already exist, from bottom to top.
+     * When a node has only one child (the left one), its value is the hash of that
+     * node and the zero value of that level. Otherwise, the hash of the children
+     * is calculated.
+     * @param leaf The new leaf to be inserted in the tree.
      */
     public insert(leaf: IMTNode) {
         checkParameter(leaf, "leaf", "number", "string", "bigint")
@@ -170,18 +204,18 @@ export default class IMT {
     }
 
     /**
-     * Deletes a leaf from the tree. It does not remove the leaf from
-     * the data structure. It set the leaf to be deleted to a zero value.
-     * @param index Index of the leaf to be deleted.
+     * It deletes a leaf from the tree. It does not remove the leaf from
+     * the data structure, but rather it sets the leaf to be deleted to the zero value.
+     * @param index The index of the leaf to be deleted.
      */
     public delete(index: number) {
         this.update(index, this.zeroes[0])
     }
 
     /**
-     * Updates a leaf in the tree.
-     * @param index Index of the leaf to be updated.
-     * @param newLeaf New leaf value.
+     * It updates a leaf in the tree. It's very similar to the {@link IMT#insert} function.
+     * @param index The index of the leaf to be updated.
+     * @param newLeaf The new leaf to be inserted.
      */
     public update(index: number, newLeaf: IMTNode) {
         checkParameter(index, "index", "number")
@@ -216,9 +250,10 @@ export default class IMT {
     }
 
     /**
-     * Creates a proof of membership.
-     * @param index Index of the proof's leaf.
-     * @returns Proof object.
+     * It creates a {@link IMTMerkleProof} for a leaf of the tree.
+     * That proof can be verified by this tree using the same hash function.
+     * @param index The index of the leaf for which a Merkle proof will be generated.
+     * @returns The Merkle proof of the leaf.
      */
     public createProof(index: number): IMTMerkleProof {
         checkParameter(index, "index", "number")
@@ -256,9 +291,10 @@ export default class IMT {
     }
 
     /**
-     * Verifies a proof and return true or false.
-     * @param proof Proof to be verified.
-     * @returns True or false.
+     * It verifies a {@link IMTMerkleProof} to confirm that a leaf indeed
+     * belongs to the tree.
+     * @param proof The Merkle tree proof.
+     * @returns True if the leaf is part of the tree, and false otherwise.
      */
     public verifyProof(proof: IMTMerkleProof): boolean {
         checkParameter(proof, "proof", "object")
--- packages/imt/src/imt/types/index.ts
+++ packages/imt/src/imt/types/index.ts
@@ -1,7 +1,24 @@
+/**
+ * It represents a node of the tree, which can be any value.
+ */
 export type IMTNode = any
 
+/**
+ * The hash function is used to compute the nodes of the tree.
+ * In a binary Merkle tree, each node is the hash of its two children.
+ */
 export type IMTHashFunction = (values: IMTNode[]) => IMTNode
 
+/**
+ * The Merkle Proof contains the necessary parameters to enable the
+ * verifier to be certain that a leaf belongs to the tree. Given the value
+ * of the leaf and its index, it is possible to traverse the tree by
+ * recalculating the hashes up to the root and using the node siblings.
+ * If the calculated root matches the root in the proof, then the leaf
+ * belongs to the tree. It's important to note that the function used
+ * to generate the proof and the one used to verify it must use the
+ * same hash function.
+ */
 export type IMTMerkleProof = {
     root: any
     leaf: any
