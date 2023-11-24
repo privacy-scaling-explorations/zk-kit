0afa086131375ff6e86ba1de1e0457eb1f9bd9ce
cedoor <me@cedoor.dev>
2023-11-24 17:22:58 +0000

    docs(poseidon-proof): add comments to main functions

--- packages/poseidon-proof/src/generate.ts
+++ packages/poseidon-proof/src/generate.ts
@@ -8,12 +8,27 @@ import { BigNumberish, PoseidonProof, SnarkArtifacts } from "./types"
 import { isBrowser, isNode } from "./utils"
 
 /**
+ * Creates a zero-knowledge proof to prove that you have the pre-image or
+ * the original message of a hash, without disclosing the actual message itself.
+ * The use of a scope parameter along with a nullifier helps ensure the uniqueness
+ * and non-reusability of the proofs, enhancing security in applications like
+ * blockchain transactions or private data verification.
+ * If, for example, this package were used with Semaphore to demonstrate possession
+ * of a Semaphore identity of a group of voters, the scope could be the poll's ID.
+ * @param message The message (or pre-image) of the hash.
+ * @param scope A public value used to contextualize the cryptographic proof
+ * and calculate the nullifier.
+ * @param snarkArtifacts The Snark artifacts (wasm and zkey files) generated in
+ * a trusted setup of the circuit are necessary to generate valid proofs
+ * @returns The Poseidon zero-knowledge proof.
  */
 export default async function generate(
     message: BigNumberish,
     scope: BigNumberish,
     snarkArtifacts?: SnarkArtifacts
 ): Promise<PoseidonProof> {
+    // If the Snark artifacts are not defined they will be automatically downloaded.
+    /* istanbul ignore next */
     if (!snarkArtifacts) {
         if (isNode()) {
             const tmpDir = libraryName
@@ -37,6 +52,7 @@ export default async function generate(
         }
     }
 
+    /* istanbul ignore next */
     if (!snarkArtifacts) {
         throw new Error("Error: Missing Snark artifacts. Please ensure all necessary Snark artifacts are included.")
     }
@@ -51,9 +67,9 @@ export default async function generate(
     )
 
     return {
-        scope: publicSignals[0],
+        scope: publicSignals[2],
         hash: publicSignals[1],
-        nullifier: publicSignals[2],
+        nullifier: publicSignals[0],
         proof: packProof(proof)
     }
 }
--- packages/poseidon-proof/src/verify.ts
+++ packages/poseidon-proof/src/verify.ts
@@ -4,10 +4,13 @@ import verificationKey from "./verification-key.json"
 import { PoseidonProof } from "./types"
 
 /**
+ * Verifies that a Poseidon proof is valid.
+ * @param poseidonProof The Poseidon zero-knowledge proof.
+ * @returns True if the proof is valid, false otherwise.
  */
 export default function verify({ scope, hash, nullifier, proof }: PoseidonProof): Promise<boolean> {
     return _verify(verificationKey, {
-        publicSignals: [scope, hash, nullifier],
+        publicSignals: [nullifier, hash, scope],
         proof: unpackProof(proof)
     })
 }
