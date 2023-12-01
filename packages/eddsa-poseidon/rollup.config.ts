2404d23554737d4c7944eeef73ebae9fd88a8a45
cedoor <me@cedoor.dev>
2023-12-01 12:58:25 +0000

    chore(eddsa-poseidon): remove iife bundles

--- packages/eddsa-poseidon/rollup.config.ts
+++ packages/eddsa-poseidon/rollup.config.ts
@@ -2,7 +2,6 @@ import commonjs from "@rollup/plugin-commonjs"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import fs from "fs"
 import cleanup from "rollup-plugin-cleanup"
-import { terser } from "rollup-plugin-terser"
 import typescript from "rollup-plugin-typescript2"
 
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"))
@@ -15,23 +14,9 @@ const banner = `/**
  * @see [Github]{@link ${pkg.homepage}}
 */`
 
-const name = pkg.name.split("/")[1].replace(/[-/]./g, (x: string) => x.toUpperCase()[1])
-
 export default {
     input: "src/index.ts",
     output: [
-        {
-            file: pkg.iife,
-            name,
-            format: "iife",
-            banner
-        },
-        {
-            file: pkg.unpkg,
-            name,
-            format: "iife",
-            plugins: [terser({ output: { preamble: banner } })]
-        },
         { file: pkg.exports.require, format: "cjs", banner },
         { file: pkg.exports.import, format: "es", banner }
     ],
