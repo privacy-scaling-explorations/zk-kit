8767dac4e92ac2de47aa68b6786735b5c075e5ed
ctrlc03 <93448202+ctrlc03@users.noreply.github.com>
2023-12-28 14:09:53 +0000

    feat(poseidon-cipher-ts): convert poseidon constants to ts and remove fs/path deps

--- packages/poseidon-cipher/rollup.config.ts
+++ packages/poseidon-cipher/rollup.config.ts
@@ -1,7 +1,6 @@
 import typescript from "rollup-plugin-typescript2"
 import fs from "fs"
 import cleanup from "rollup-plugin-cleanup"
-import { terser } from "rollup-plugin-terser"
 
 const pkg = JSON.parse(fs.readFileSync("./package.json", "utf8"))
 const banner = `/**
@@ -12,23 +11,10 @@ const banner = `/**
  * @license ${pkg.license}
  * @see [Github]{@link ${pkg.homepage}}
 */`
-const name = pkg.name.substr(1).replace(/[-/]./g, (x: string) => x.toUpperCase()[1])
 
 export default {
     input: "src/index.ts",
     output: [
-        {
-            file: pkg.iife,
-            name,
-            format: "iife",
-            banner
-        },
-        {
-            file: pkg.unpkg,
-            name,
-            format: "iife",
-            plugins: [terser({ output: { preamble: banner } })]
-        },
         { file: pkg.exports.require, format: "cjs", banner },
         { file: pkg.exports.import, format: "es", banner }
     ],
