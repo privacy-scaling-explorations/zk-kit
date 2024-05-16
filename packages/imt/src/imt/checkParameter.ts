37fd682550dae95aa18ece52bdbb8618be13494e
Cedoor <me@cedoor.dev>
2024-05-16 11:22:08 +0100

    refactor(imt)!: create two different imt libs (#285)
    IMT implementations have been separated to allow them to be versioned more precisely.
    
    re #282

--- packages/imt/src/imt/checkParameter.ts
+++
@@ -1,9 +0,0 @@
-export default function checkParameter(value: any, name: string, ...types: string[]) {
-    if (value === undefined) {
-        throw new TypeError(`Parameter '${name}' is not defined`)
-    }
-
-    if (!types.includes(typeof value)) {
-        throw new TypeError(`Parameter '${name}' is none of these types: ${types.join(", ")}`)
-    }
-}
