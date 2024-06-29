d7ce37379e8697321f6093a0b9664a97a5edee6c
Arch0125 <dasarchisman25@gmail.com>
2024-06-30 02:47:53 +0530

    chore: minor fixes

--- packages/imt-rs/src/lib.rs
+++ packages/imt-rs/src/lib.rs
@@ -1,28 +1,24 @@
-use std::error::Error;
-
 use tiny_keccak::{Hasher, Keccak};
 
 pub struct IMT {
-    nodes: Vec<Vec<Option<IMTNode>>>,
+    nodes: Vec<Vec<IMTNode>>,
     zeroes: Vec<IMTNode>,
     hash: IMTHashFunction,
     depth: usize,
     arity: usize,
 }
 
-struct IMTMerkleProof {
+pub struct IMTMerkleProof {
     root: IMTNode,
     leaf: IMTNode,
     path_indices: Vec<usize>,
     siblings: Vec<Vec<IMTNode>>,
-    leaf_index: usize,
 }
 
 
 impl IMT {
-    pub fn new(hash: IMTHashFunction, depth: usize, zero_value: IMTNode, arity: usize, leaves: Option<Vec<IMTNode>>) -> Result<IMT, &'static str> {
-
-        if leaves.is_some() && leaves.as_ref().unwrap().len() > arity.pow(depth as u32) {
+    pub fn new(hash: IMTHashFunction, depth: usize, zero_value: IMTNode, arity: usize, leaves: Vec<IMTNode>) -> Result<IMT, &'static str> {
+        if leaves.len() > arity.pow(depth as u32) {
             return Err("The tree cannot contain more than arity^depth leaves");
         }
 
@@ -40,19 +36,17 @@ impl IMT {
             current_zero = (imt.hash)(vec![current_zero; arity]);
         }
 
-        if let Some(leaves) = leaves {
-            imt.nodes[0] = leaves.into_iter().map(Some).collect();
+        imt.nodes[0] = leaves;
 
-            for level in 0..depth {
-                for index in 0..((imt.nodes[level].len() as f64 / arity as f64).ceil() as usize) {
-                    let position = index * arity;
-                    let children: Vec<_> = (0..arity)
-                        .map(|i| imt.nodes[level].get(position + i).cloned().flatten().unwrap_or_else(|| imt.zeroes[level].clone()))
-                        .collect();
+        for level in 0..depth {
+            for index in 0..((imt.nodes[level].len() as f64 / arity as f64).ceil() as usize) {
+                let position = index * arity;
+                let children: Vec<_> = (0..arity)
+                    .map(|i| imt.nodes[level].get(position + i).cloned().unwrap_or_else(|| imt.zeroes[level].clone()))
+                    .collect();
 
-                    if let Some(next_level) = imt.nodes.get_mut(level + 1) {
-                        next_level.push(Some((imt.hash)(children)));
-                    }
+                if let Some(next_level) = imt.nodes.get_mut(level + 1) {
+                    next_level.push((imt.hash)(children));
                 }
             }
         }
@@ -60,68 +54,57 @@ impl IMT {
         Ok(imt)
     }
 
-    pub fn root(&mut self) -> Option<String>  {
-        let nodes = self.nodes[self.depth].clone();
-        return nodes[0].clone();
+    pub fn root(&mut self) -> Option<IMTNode>  {
+       self.nodes[self.depth].first().cloned()
     }
 
-    pub fn depth(&mut self) -> usize {
-        return self.depth;
+    pub fn depth(&self) -> usize {
+    self.depth
     }
 
-    pub fn leaves(&mut self) -> Vec<Option<String>> {
-        return self.nodes[0].clone();
+    pub fn leaves(&self) -> Vec<IMTNode> {
+        self.nodes[0].clone()
     }
 
-    pub fn arity(&mut self) -> usize {
-        return self.arity;
+    pub fn arity(&self) -> usize {
+        self.arity
     }
 
     pub fn insert(&mut self, leaf: IMTNode) -> Result<(), &'static str> {
-
         if self.nodes[0].len() >= self.arity.pow(self.depth as u32) {
             return Err("The tree is full");
         }
 
-        let mut node = leaf;
-        let mut index = self.nodes[0].len();
+        let index = self.nodes[0].len();
+        self.nodes[0].push(leaf);
+        self.update(index, self.nodes[0][index].clone())
+    }
+
+    pub fn update(&mut self, mut index: usize, new_leaf: IMTNode) -> Result<(), &'static str> {
+        if index >= self.nodes[0].len() {
+            return Err("The leaf does not exist in this tree");
+        }
+
+        let mut node = new_leaf;
+        self.nodes[0][index] = node.clone();
 
         for level in 0..self.depth {
             let position = index % self.arity;
             let level_start_index = index - position;
             let level_end_index = level_start_index + self.arity;
 
-            let mut children = Vec::with_capacity(self.arity);
-
-            if level >= self.nodes.len() {
-                self.nodes.push(Vec::new());
-            }
-
-            if self.nodes[level].len() <= index {
-                self.nodes[level].resize(index + 1, None);
-            }
-            self.nodes[level][index] = Some(node);
-
-            for i in level_start_index..level_end_index {
-                if i < self.nodes[level].len() {
-                    children.push(self.nodes[level][i].clone().unwrap_or_else(|| self.zeroes[level].clone()));
-                } else {
-                    children.push(self.zeroes[level].clone());
-                }
-            }
+            let children: Vec<_> = (level_start_index..level_end_index)
+                .map(|i| self.nodes[level].get(i).cloned().unwrap_or_else(|| self.zeroes[level].clone()))
+                .collect();
 
             node = (self.hash)(children);
-            index = index / self.arity;
-        }
+            index /= self.arity;
 
-        if self.nodes.len() == self.depth {
-            self.nodes.push(Vec::new());
-        }
-
-        if self.nodes[self.depth].is_empty() {
-            self.nodes[self.depth].push(Some(node));
-        } else {
-            self.nodes[self.depth][0] = Some(node);
+            if self.nodes[level + 1].len() <= index {
+                self.nodes[level + 1].push(node.clone());
+            } else {
+                self.nodes[level + 1][index] = node.clone();
+            }
         }
 
         Ok(())
@@ -129,47 +112,6 @@ impl IMT {
 
     pub fn delete(&mut self, index: usize) -> Result<(), &'static str> {
         self.update(index, self.zeroes[0].clone())
-    }
-
-    pub fn update(&mut self, mut index: usize, new_leaf: IMTNode) -> Result<(), &'static str> {
-
-        if index >= self.nodes[0].len() {
-            return Err("The leaf does not exist in this tree");
-        }
-
-        let mut node = new_leaf;
-
-        for level in 0..self.depth {
-            let position = index % self.arity;
-            let level_start_index = index - position;
-            let level_end_index = level_start_index + self.arity;
-
-            let mut children = Vec::with_capacity(self.arity);
-
-            if self.nodes[level].len() <= index {
-                self.nodes[level].resize(index + 1, None);
-            }
-            self.nodes[level][index] = Some(node);
-
-            for i in level_start_index..level_end_index {
-                if i < self.nodes[level].len() {
-                    children.push(self.nodes[level][i].clone().unwrap_or_else(|| self.zeroes[level].clone()));
-                } else {
-                    children.push(self.zeroes[level].clone());
-                }
-            }
-
-            node = (self.hash)(children);
-            index = index / self.arity;
-        }
-
-        if self.nodes[self.depth].is_empty() {
-            self.nodes[self.depth].push(Some(node));
-        } else {
-            self.nodes[self.depth][0] = Some(node);
-        }
-
-        Ok(())
     }
 
     pub fn create_proof(&self, index: usize) -> Result<IMTMerkleProof, &'static str> {
@@ -191,7 +133,7 @@ impl IMT {
 
             for i in level_start_index..level_end_index {
                 if i != current_index {
-                    level_siblings.push(self.nodes[level].get(i).cloned().flatten().unwrap_or_else(|| self.zeroes[level].clone()));
+                    level_siblings.push(self.nodes[level].get(i).cloned().unwrap_or_else(|| self.zeroes[level].clone()));
                 }
             }
 
@@ -200,11 +142,10 @@ impl IMT {
         }
 
         Ok(IMTMerkleProof {
-            root: self.nodes[self.depth][0].clone().unwrap_or_default(),
-            leaf: self.nodes[0][index].clone().unwrap_or_default(),
+            root: self.nodes[self.depth][0].clone(),
+            leaf: self.nodes[0][index].clone(),
             path_indices,
             siblings,
-            leaf_index: index,
         })
     }
 
@@ -238,7 +179,6 @@ pub fn keccak256_hash_function(nodes: Vec<String>) -> String {
 
     hex::encode(result)
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -250,7 +190,7 @@ mod tests {
     #[test]
     fn test_new_imt() {
         let hash: IMTHashFunction = simple_hash_function;
-        let imt = IMT::new(hash, 3, "zero".to_string(), 2, None);
+        let imt = IMT::new(hash, 3, "zero".to_string(), 2, vec![]);
 
         assert!(imt.is_ok());
     }
@@ -258,7 +198,7 @@ mod tests {
     #[test]
     fn test_insertion() {
         let hash: IMTHashFunction = simple_hash_function;
-        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, None).unwrap();
+        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, vec![]).unwrap();
 
         assert!(imt.insert("leaf1".to_string()).is_ok());
     }
@@ -266,7 +206,7 @@ mod tests {
     #[test]
     fn test_delete() {
         let hash: IMTHashFunction = simple_hash_function;
-        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, Some(vec!["leaf1".to_string()])).unwrap();
+        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, vec!["leaf1".to_string()]).unwrap();
 
         assert!(imt.delete(0).is_ok());
     }
@@ -274,7 +214,7 @@ mod tests {
     #[test]
     fn test_update() {
         let hash: IMTHashFunction = simple_hash_function;
-        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, Some(vec!["leaf1".to_string()])).unwrap();
+        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, vec!["leaf1".to_string()]).unwrap();
 
         assert!(imt.update(0, "new_leaf".to_string()).is_ok());
     }
@@ -282,7 +222,7 @@ mod tests {
     #[test]
     fn test_create_and_verify_proof() {
         let hash: IMTHashFunction = simple_hash_function;
-        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, Some(vec!["leaf1".to_string()])).unwrap();
+        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, vec!["leaf1".to_string()]).unwrap();
         imt.insert("leaf2".to_string()).unwrap();
 
         let proof = imt.create_proof(0);
@@ -296,14 +236,14 @@ mod tests {
     fn should_not_initialize_with_too_many_leaves() {
         let hash: IMTHashFunction = simple_hash_function;
         let leaves = vec!["leaf1".to_string(), "leaf2".to_string(), "leaf3".to_string(), "leaf4".to_string(), "leaf5".to_string()];
-        let imt = IMT::new(hash, 2, "zero".to_string(), 2, Some(leaves));
+        let imt = IMT::new(hash, 2, "zero".to_string(), 2, leaves);
         assert!(imt.is_err());
     }
 
     #[test]
     fn should_not_insert_in_full_tree() {
         let hash: IMTHashFunction = simple_hash_function;
-        let mut imt = IMT::new(hash, 1, "zero".to_string(), 2, Some(vec!["leaf1".to_string(), "leaf2".to_string()])).unwrap();
+        let mut imt = IMT::new(hash, 1, "zero".to_string(), 2, vec!["leaf1".to_string(), "leaf2".to_string()]).unwrap();
 
         let result = imt.insert("leaf3".to_string());
         assert!(result.is_err());
@@ -312,9 +252,34 @@ mod tests {
     #[test]
     fn should_not_delete_nonexistent_leaf() {
         let hash: IMTHashFunction = simple_hash_function;
-        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, Some(vec!["leaf1".to_string()])).unwrap();
+        let mut imt = IMT::new(hash, 3, "zero".to_string(), 2, vec!["leaf1".to_string()]).unwrap();
 
         let result = imt.delete(1); 
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_root() {
+        let hash: IMTHashFunction = simple_hash_function;
+        let mut imt = IMT::new(hash, 2, "zero".to_string(), 2, vec!["leaf1".to_string(), "leaf2".to_string()]).unwrap();
+
+        assert_eq!(imt.root(), Some("leaf1,leaf2,zero,zero".to_string()));
+    }
+
+    #[test]
+    fn test_leaves() {
+        let hash: IMTHashFunction = simple_hash_function;
+        let imt = IMT::new(hash, 2, "zero".to_string(), 2, vec!["leaf1".to_string(), "leaf2".to_string()]).unwrap();
+
+        assert_eq!(imt.leaves(), vec!["leaf1".to_string(), "leaf2".to_string()]);
+    }
+
+    #[test]
+    fn test_depth_and_arity() {
+        let hash: IMTHashFunction = simple_hash_function;
+        let imt = IMT::new(hash, 3, "zero".to_string(), 2, vec![]).unwrap();
+
+        assert_eq!(imt.depth(), 3);
+        assert_eq!(imt.arity(), 2);
     }
 }
