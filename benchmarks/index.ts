93efa6b7e803c3ab4e72fac8e97ea63f347d7306
prodasl <prodasl@hotmail.com>
2024-09-21 11:52:05 -0500

    refactor(benchmarks): added winston to package.json and refactored index.ts
    Added winston to package.json and refactored index.ts to
    
    run the samples with a loop managed by an array
    
    re #311

--- benchmarks/index.ts
+++ benchmarks/index.ts
@@ -1,16 +1,8 @@
 import run from "./benchmark-merkle-trees"
 
-// Benchmark with 8 leafs
-let treeDepth = 3
-let numberOfLeaves = 2 ** treeDepth
-run(treeDepth, numberOfLeaves)
-
-// Benchmark with 128 leafs
-treeDepth = 7
-numberOfLeaves = 2 ** treeDepth
-run(treeDepth, numberOfLeaves)
-
-// Benchmark with 1024 leafs
-treeDepth = 10
-numberOfLeaves = 2 ** treeDepth
-run(treeDepth, numberOfLeaves)
+const treeDepthSamples: number[] = [3, 7, 10]
+let numberOfLeaves: number
+treeDepthSamples.forEach((treeDepth) => {
+    numberOfLeaves = 2 ** treeDepth
+    run(treeDepth, numberOfLeaves)
+})
