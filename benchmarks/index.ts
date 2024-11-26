98004964281b44b1fff46e5897b893c1ce35fb1a
witty <131909329+0xwitty@users.noreply.github.com>
2024-11-26 21:48:36 +0300

    Fix for loop structure to improve readability

--- benchmarks/index.ts
+++ benchmarks/index.ts
@@ -3,6 +3,7 @@ import run from "./benchmark-merkle-trees"
 const treeDepthSamples: number[] = [3, 7, 10]
 let numberOfLeaves: number
 treeDepthSamples.forEach((treeDepth) => {
-    numberOfLeaves = 2 ** treeDepth
-    run(treeDepth, numberOfLeaves)
-})
+    const numberOfLeaves = 2 ** treeDepth;
+    run(treeDepth, numberOfLeaves);
+});
+
